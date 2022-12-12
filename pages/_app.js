0cdffc534e8a7d53f7b4531171b13dce382da7c8
Esref Bozkurt <113592929+esrefbozkurt@users.noreply.github.com>
2022-12-12 14:37:10 +0100

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import GlobalStyles from "../components/GlobalStyles";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
