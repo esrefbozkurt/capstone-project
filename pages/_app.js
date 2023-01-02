2581349aaa0d4a01166b2fbad8fe23365602432f
Esref Bozkurt <esref.bozkurt@gmx.de>
2023-01-02 06:47:58 +0100

    add custom 404, create favourites page and button

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import GlobalStyles from "../components/GlobalStyles";
+import Layout from "../components/Layout/Layout";
 import { dataBiceps } from "../lib/data";
 
 function MyApp({ Component, pageProps }) {
   const [isFavourite, setIsFavourite] = useState([]);
 
-  function handleFav(id) {
-    isFav.includes(id)
+  function handleFav(id, event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isFavourite.includes(id)
       ? setIsFavourite((prev) => prev.filter((favour) => favour !== id))
       : setIsFavourite((prev) => [...prev, id]);
+
+    console.log(id);
+    console.log(...prev);
+    console.log(favour);
   }
 
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} onFav={handleFav} isFavourite={isFavourite} />
+      <Layout>
+        <Component {...pageProps} onFav={handleFav} isFavourite={isFavourite} />
+      </Layout>
     </>
   );
 }
