2db0d570593b33efd5728cb5ac84c78864f84a65
Esref Bozkurt <esref.bozkurt@gmx.de>
2023-01-22 13:51:02 +0100

    update input component and render method

---
+++ components/Main/Workouts/WorkoutDetails/FormAddSets/StyledAddSets.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const StyledAddSets = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+  }
+  /* align-items: center;
+  justify-content: center;
+  background-color: white;
+  align-items: center;
+  /* border-bottom-left-radius: 0.5rem;
+border-bottom-right-radius: 0.5rem;
+padding-bottom: 0.5rem; */
+
+  /* border: black;
+  border-radius: 0.3rem;
+
+  width: 100%; */
+`;
+
+export const StyledForm = styled.form`
+  fieldset {
+  }
+
+  .set-input {
+    display: flex;
+    padding: 0.4rem 0.4rem 0.4rem 0;
+    background-color: black;
+    font-size: 1.1rem;
+    /* border: black solid 1px; */
+    border-radius: 0.5rem;
+    color: white;
+    padding: 0.4rem;
+  }
+`;
