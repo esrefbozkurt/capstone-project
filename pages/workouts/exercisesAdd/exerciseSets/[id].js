7ca134ba20976af3738e855c28569996b7710492
Esref Bozkurt <esref.bozkurt@gmx.de>
2023-01-22 04:50:08 +0100

    finish toggle add ui an function

---
+++ pages/workouts/exercisesAdd/exerciseSets/[id].js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import AddExercises from "../../../components/Main/Workouts/WorkoutDetails/AddExercises/AddExercises";
+import Head from "next/head";
+
+const ExerciseAdd = ({
+  workouts,
+  exercises,
+  onAddExercise,
+  exerciseAdded,
+  updateAddedExercises,
+}) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const currentExercise = workouts.find((workout) => workout.id === id);
+
+  if (!currentWorkout) {
+    return null;
+  }
+
+  const name = currentWorkout?.name;
+
+  return (
+    <>
+      <Head>
+        <title>TrackFit | {name}</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, "
+        />
+      </Head>
+      <AddExercises
+        exercises={exercises}
+        currentWorkout={currentWorkout}
+        onAddExercise={onAddExercise}
+        id={id}
+        exerciseAdded={exerciseAdded}
+        updateAddedExercises={updateAddedExercises}
+      />
+    </>
+  );
+};
+
+export default ExerciseAdd;
