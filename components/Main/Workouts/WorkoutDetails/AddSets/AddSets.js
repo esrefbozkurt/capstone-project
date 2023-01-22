8361253417a0b5e0d3f72ca57d2661a3038eebb3
Esref Bozkurt <esref.bozkurt@gmx.de>
2023-01-22 07:06:59 +0100

    delete unused component

--- components/Main/Workouts/WorkoutDetails/AddSets/AddSets.js
+++ components/Main/Workouts/WorkoutDetails/AddSets/AddSets.js
@@ -4,21 +4,21 @@ export default function AddSets({ exerciseAdded, id }) {
   console.log(exerciseAdded);
   return (
     <>
-      {/* <StyledAddSetsHeader> */}
-      <Link href={`/workouts/${id}`}>
-        <Image
-          src="/arrow-circle-left.svg"
-          width={24}
-          height={24}
-          alt="back button"
-          priority
-        />
-      </Link>
-      <h2 className="headerAddExercise">
-        Add Exercises to {exerciseAdded.name}
-      </h2>
-      {/* </StyledAddSetsHeader>
-      <StyledAddSets></StyledAddSets> */}
+      <StyledAddSetsHeader>
+        <Link href={`/workouts/${id}`}>
+          <Image
+            src="/arrow-circle-left.svg"
+            width={24}
+            height={24}
+            alt="back button"
+            priority
+          />
+        </Link>
+        <h2 className="headerAddExercise">
+          Add Exercises to {exerciseAdded.name}
+        </h2>
+      </StyledAddSetsHeader>
+      <StyledAddSets></StyledAddSets>
     </>
   );
 }
